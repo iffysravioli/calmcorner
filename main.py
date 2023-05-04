a37cb8cbb5223173c53da971fe80d3ec3f795c49
iffysravioli <21itmj@gmail.com>
2023-05-03 23:07:14 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    userentries = []
+
+    while True: 
+        print("Calm Corner")
+        print("1. New entry")
+        print("2. Save entry")
+        print("3. Exit Calm Corner")
